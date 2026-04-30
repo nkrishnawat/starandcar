bee438129cf56e147e5f175efba73cf00b54d308
nkrishnawat <n.s.krishnawat@gmail.com>
2026-04-30 10:30:15 +0530

    Fixes

--- src/main/java/com/resilientechnology/starandcar/StarandcarApplication.java
+++ src/main/java/com/resilientechnology/starandcar/StarandcarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class StarandcarApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(StarandcarApplication.class, args);
